f46fd3003b6f253e6981a10ff73633bd8eb82871
Keerthivasan <keerthityou79@gmail.com>
2022-11-22 17:20:02 +0530

    added frontend api

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from 'dotenv'
+import postRoutes from './routes/posts.js'
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts',postRoutes);
+
+const CONNECTION_URL =process.env.CONNECTION_URL || 
+  "mongodb+srv://Keerthivasan:vasan2012@cluster0.csnpfrb.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+const connect = async () => {
+  try {
+    await mongoose.connect(CONNECTION_URL);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+app.listen(PORT, () => {
+  connect();
+  console.log("Connected to backend.");
+});
